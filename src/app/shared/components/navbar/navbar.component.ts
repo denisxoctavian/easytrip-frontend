ba8b1b1b96a8d9df842dc9047c40399706563011
denisxoctavian <denis.octavian21@gmail.com>
2025-03-12 23:26:51 +0200

    -added sidenav for mobile display

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule,MatSidenavModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
