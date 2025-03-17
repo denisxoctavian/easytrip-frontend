5389dfc7356dba07dccb4f2e6f60f391948711b8
denisxoctavian <denis.octavian21@gmail.com>
2025-03-17 22:40:56 +0200

    -fixed theme issue & hopefully fixed navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from '../../../features/login/login.component';
+
 
 
 
@@ -15,12 +16,13 @@ import { LoginComponent } from '../../../features/login/login.component';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  
   readonly dialog = inject(MatDialog);
 
   //Open login dialog
   openDialog(){
       this.dialog.open(LoginComponent, {width:'500px',height:'300px',panelClass: ['animate__animated','animate__fadeInDown']},)
     }
+
 }
 
