5b67432390e375a1d7f540da4e4bf4838496ec43
denisxoctavian <denis.octavian21@gmail.com>
2025-03-19 22:41:17 +0200

    -hopefully environment is fixed now

---
+++ src/app/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production:true,
+    APP_ENVIRONMENT: 'PROD',
+    APP_PATH: 'https://easytrip-z9dh.onrender.com'
+};
--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production:true,
-    APP_ENVIRONMENT: 'PROD',
-    APP_PATH: 'https://easytrip-z9dh.onrender.com'
+    production:false,
+    APP_ENVIRONMENT: 'LOCAL',
+    APP_PATH: 'http://localhost:8080'
 };
